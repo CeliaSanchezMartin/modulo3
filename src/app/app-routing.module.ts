5e8d0be328554398274b552ea7df9db176aca61e
Celia Sánchez <celiasanchezmartin6@gmail.com>
2023-02-28 13:29:59 +0100

    finish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
+  {path: "", component: HomeComponent },
+  {path: "register", component: RegisterComponent }
  
 ];
 
